6914278307eaa5f3b70df1b8b6cd8c5fe7d0e10a
Nesta-CZ <michalovsky810@gmail.com>
2015-02-24 21:17:29 +0100

    as
    as

---
+++ src/public/views.py
@@ -0,0 +1,19 @@
+"""
+Logic for dashboard related routes
+"""
+from flask import Blueprint, render_template
+from .forms import LogUserForm
+from ..data.models import LogUser
+blueprint = Blueprint('public', __name__)
+
+@blueprint.route('/', methods=['GET'])
+def index():
+    return render_template('public/index.tmpl')
+
+@blueprint.route('/loguserinput', methods=['GET','POST'])
+def InsertLogUser():
+    form = LogUserForm()
+    if form.validate_on_submit():
+        LogUser.create(**form.data)
+    return render_template('public/LogUser.tmpl',form=form)
+
